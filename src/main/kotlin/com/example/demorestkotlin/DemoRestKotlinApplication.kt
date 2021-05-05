dc6db24335cba89ed1bd061ab52dc34548767b50
subhankar biswas <sub.nitt@yahoo.com>
2021-05-05 20:49:02 +0530

    init

---
+++ src/main/kotlin/com/example/demorestkotlin/DemoRestKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.example.demorestkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoRestKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<DemoRestKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/demorestkotlin/DemoRestKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.demorestkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoRestKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
